5d9f53107565494a6cbd7affa2dcbf580500bc4c
Brian Ford <btford@umich.edu>
2013-10-22 12:10:23 -0700

    update

--- step-3/test/unit/controllersSpec.js
+++ step-3/test/unit/controllersSpec.js
@@ -7,12 +7,12 @@ describe('PhoneCat controllers', function() {
 
     beforeEach(module('phonecatApp'));
 
-    it('should create "phones" model with 3 phones', function() {
+    it('should create "phones" model with 3 phones', inject(function($controller) {
       var scope = {},
-          ctrl = new PhoneListCtrl(scope);
+          ctrl = $controller('PhoneListCtrl', {$scope:scope});
 
       expect(scope.phones.length).toBe(3);
-    });
+    }));
 
     it('should create "phones" model with 3 phones', inject(function($controller) {
       var scope = {},
